39eefe820a3cc55ce2a2ea8a74854698514b8fc1
WangShaoBin <wangshaobin@arrail-dental.com>
2018-10-23 20:04:35 +0800

    增加实体类

--- src/main/java/com/app/entity/OrderDetails.java
+++ src/main/java/com/app/entity/OrderDetails.java
@@ -1,33 +1,16 @@
 package com.app.entity;
 
-public class OrderDetails {
-    private Long id;
+import com.app.base.BaseEntity;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class OrderDetails extends BaseEntity implements Serializable {
+
 
     private Long orderId;
 
     private Long productId;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
 }
